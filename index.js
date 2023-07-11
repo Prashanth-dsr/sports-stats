0ebe9aba8ef19e726b1c870376c8bfd6517dbc90
Dasari Prashanth <prashanth60238@gmail.com>
2023-07-11 12:20:25 +0530

    another commit

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import delay from "delay";
 import PQueue from "p-queue";
 
-const queue = 
-
-new PQueue({ concurrency: 1 });
+const queue = new 
+({ concurrency: 1 });
 
 (async () => {
   await delay(200);
